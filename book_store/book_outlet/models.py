f7067faa8c6a866d17bed287d8fffff15bdf5bfd
Deshmukh P <deshmukh.p@matchbot.gg>
2024-08-13 17:26:57 +0530

    update book model with more fields and new migrations

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,17 +1,20 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    rating = models.IntegerField()
-    # author = models.CharField(max_length=200)
+    rating = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
+    author = models.CharField(null=True, max_length=200)
+    is_bestselling = models.BooleanField(default=False)
     # price = models.DecimalField(max_digits=6, decimal_places=2)
     # genre = models.CharField(max_length=200)
     # publication_date = models.DateField()
     # description = models.TextField()
     # image = models.ImageField(upload_to='book_images/')
 
-    # def __str__(self):
-    #     return self.title
+    def __str__(self):
+        return f"{self.title} ({self.rating})"
