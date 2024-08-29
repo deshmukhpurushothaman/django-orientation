083e89d30b6bcc1e9910d86ee2214b9f4744b5f5
Deshmukh P <deshmukh.p@matchbot.gg>
2024-08-29 08:05:36 +0530

    basic feedback form setup with has_error only

---
+++ feedback/reviews/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def review(request):
+    if request.method == 'POST':
+        entered_username = request.POST['username']
+
+        if entered_username == "" and len(entered_username) >= 100:
+            return render(request, 'reviews/review.html', {
+                'has_error': True
+            })
+        return HttpResponseRedirect('/thank_you')
+    return render(request, 'reviews/review.html', {
+        'has_error': False
+    })
+
+
+def thank_you(request):
+    return render(request, 'reviews/thank_you.html')
