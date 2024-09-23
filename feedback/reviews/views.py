27484a3f5655f6dc8638d4eff858273d015add0b
Deshmukh P <deshmukh.p@matchbot.gg>
2024-09-23 08:48:21 +0530

    class views and views offered by django

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,31 +1,43 @@
-from django.http import HttpResponseRedirect
+from typing import Any
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from .forms import ReviewForm
-from .models import Review
 from django.views import View
+from django.views.generic.base import TemplateView
+from .models import Review
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 
 # Create your views here.
 
 
-class ReviewView(View):
-    def post(self, request):
-        form = ReviewForm(request.POST)
-
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/thank_you')
-
-        return render(request, 'reviews/review.html', {
-            'form': form
-        })
-
-    def get(self, request):
-        form = ReviewForm()
-
-        return render(request, 'reviews/review.html', {
-            'form': form
-        })
+class ReviewView(CreateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = 'reviews/review.html'
+    success_url = '/thank-you'
 
 
-def thank_you(request):
-    return render(request, 'reviews/thank_you.html')
+class ThankYouView(TemplateView):
+    template_name = 'reviews/thank_you.html'
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['message'] = 'This works!'
+        return context
+
+
+class ReviewList(ListView):
+    template_name = 'reviews/review_list.html'
+    model = Review
+    context_object_name = 'reviews'
+
+    # def get_queryset(self):
+    #     base_query = super().get_queryset()
+    #     data = base_query.filter(rating__gt=4)
+    #     return data
+
+
+class SingleReviewView(DetailView):
+    template_name = 'reviews/single_review.html'
+    model = Review
