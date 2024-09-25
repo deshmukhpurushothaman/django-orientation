31f818528335b8ed27a8346ec3f0cbea1188a6e9
Deshmukh P <deshmukh.p@matchbot.gg>
2024-09-25 08:36:59 +0530

    basic comments form and model setup

--- blog_site/blog/views.py
+++ blog_site/blog/views.py
@@ -1,6 +1,10 @@
 from typing import Any
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 from django.views.generic import ListView, DetailView
+
+from blog.forms import CommentForm
 from .models import Post
 
 # Create your views here.
@@ -27,10 +31,29 @@ class AllPostsView(ListView):
 
 
 class SinglePostView(DetailView):
-    template_name = "blog/post-detail.html"
-    model = Post
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm()
+        }
+        return render(request, "blog/post-detail.html", context)
 
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-        context = super().get_context_data(**kwargs)
-        context["post_tags"] = self.object.tags.all()
-        return context
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": comment_form
+        }
+        return render(request, "blog/post-detail.html", context)
